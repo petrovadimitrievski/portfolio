d0dddf06aed6e26ee9f012ae5f0d87d8db2135fc
Zore <petrovazorka@yahoo.com>
2024-04-29 21:55:56 -0500

    New portfolio page

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+// I  will delete it after Homework ( just for practice ) 
+// function stringConcat(){
+//     var Welcome="Welcome and Thank you";
+//     var Web=" for Visiting my Portfolio page";
+//     var WT=Welcome+""+Web;
+//     alert(WT);
+//     console.log(WT);
+    
+//     }
+//     stringConcat();
+
+
+
+$(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active") ;
+
+});
+
+
+// $("dropdown-item").css("color", "#07439C");
+
+
+
+
+
---
+++ js/projects.js
@@ -0,0 +1,10 @@
+
+function stringConcat(){
+    var Welcome="This page is under construction";
+    var Web="  Thank you for your patience";
+    var WT=Welcome+""+Web;
+    alert(WT);
+    console.log(WT);
+    
+    }
+    stringConcat();
